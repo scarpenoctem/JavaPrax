7d7058f9b93778e015d46f0975ce896d5f46e6ac
Jeremiah <jeremiahakins@ymail.com>
2019-02-02 15:18:14 -0500

    Jeremiah | Added isPalindrome()

--- JavaPrax/src/Utils/StringUtil.java
+++ JavaPrax/src/Utils/StringUtil.java
@@ -3,22 +3,36 @@ package Utils;
 import java.util.Arrays;
 
 public class StringUtil {
-	
-	//accepts a String and returns the String reversed
+
+	// accepts a String and returns the String reversed
 	public static String reverseString(String str) {
 		StringBuilder reversedStr = new StringBuilder();
-		for(int x = str.length()-1; x >= 0; x--) {
+		for (int x = str.length() - 1; x >= 0; x--) {
 			reversedStr.append(str.charAt(x));
 		}
 		return reversedStr.toString();
 	}
-	
-	//accepts two Strings and returns true/false based on if the two Strings are anagrams of each other
+
+	// accepts two Strings and returns true/false based on if the two Strings are
+	// anagrams of each other
 	public static boolean isAnagram(String str1, String str2) {
 		char[] sortedStr1 = str1.toCharArray();
 		char[] sortedStr2 = str2.toCharArray();
 		Arrays.sort(sortedStr1);
 		Arrays.sort(sortedStr2);
-		return Arrays.equals(sortedStr1, sortedStr2);		
+		return Arrays.equals(sortedStr1, sortedStr2);
+	}
+
+	// accepts a String and returns true/false based on if it is a palindrome
+	public static boolean isPalindrome(String str) {
+		int y = str.length()-1;
+		for (int x = 0; x < str.length(); x++) {
+			if (str.charAt(x) == str.charAt(y)) {
+				y--;
+			} else {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- JavaPrax/src/main/PracticeApp.java
+++ JavaPrax/src/main/PracticeApp.java
@@ -1,7 +1,12 @@
 package main;
 
+import Utils.StringUtil;
+
 public class PracticeApp {
 	public static void main(String args[]) {
-		System.out.println("yoooooo");
+		System.out.println("yoooooo\n\n\n");
+		
+		String str = "racecar";
+		StringUtil.isPalindrome(str);
 	}
 }
