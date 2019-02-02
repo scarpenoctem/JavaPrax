50345022c1fac057c1fc0e296b7938aa110de57b
Jeremiah <jeremiahakins@ymail.com>
2019-02-02 18:39:56 -0500

    Jeremiah | Added Interfaces package,FriendInfo class, and PersonInfo interface

--- JavaPrax/src/Utils/MapUtil.java
+++ JavaPrax/src/Utils/MapUtil.java
@@ -2,4 +2,5 @@ package Utils;
 
 public class MapUtil {
 
+	
 }
---
+++ JavaPrax/src/main/FriendInfo.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendInfo {
+
+	List<String> name = new ArrayList<>();
+	
+}
